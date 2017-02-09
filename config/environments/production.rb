a9102c230654fb27ab95f95a8b38581de805ee38
George Tzavelas <george.tzavelas@shopify.com>
2017-02-08 20:15:52 -0500

    asset precompilation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,7 +86,7 @@ Rails.application.configure do
 
   config.public_file_server.enabled = true
   config.assets.compile = true
-  config.assets.precompile =  ['*.js', '*.css', '*.css.erb']
+  config.assets.precompile += %w( * )
 
   # Host name
   config.x.full_host = "http://ftp.team2706.ca:3000"
